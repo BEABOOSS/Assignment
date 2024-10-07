068ae0fc36509ca2b82293f80f3f105129eb95ff
BEABOOSS <72757983+BEABOOSS@users.noreply.github.com>
2024-10-06 20:41:15 -0400

    Daily assignment 11

---
+++ js/wrapper.js
@@ -0,0 +1,17 @@
+
+
+const body = /** @type {HTMLBodyElement} */(document.body);
+
+body.innerHTML = `
+<div id="mainDiv">
+    The <b>World Wide Web Consortium</b>
+    , abbreviation to <strong>W3C</strong>
+    , is a
+    <a href=" http://en.wikipedia.org/wiki/Standards_organization " target="_blank" title="Standards organization">
+    Standards Organization.</a>
+    A <i>non-profit</i>
+    responsible for promoting the compatibility of the technologies
+    <a href="http://en.wikipedia.org/wiki/World_Wide_Web"
+    target="_blank" title="World Wide Web">World Wide Web</a>.
+    <p style="text-align: right">Warren</p>
+</div>`
