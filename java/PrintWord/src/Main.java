9a31f12f5812ab6db7c81db729450488bae5f27e
BEABOOSS <72757983+BEABOOSS@users.noreply.github.com>
2024-10-31 10:47:40 -0400

    Added the 'if else' version of the method

--- java/PrintWord/src/Main.java
+++ java/PrintWord/src/Main.java
@@ -2,10 +2,12 @@ public class Main {
     public static void main(String[] args) {
 
         // First part of the assignment is PrintWord
-        PrintWord.printWord();
+        PrintWord.printWordSwitch();
+        PrintWord.printWordIfElse();
+
 
         // Second part of the assignment is Creating illegal field names
-        // ErrorProne errorProne = new ErrorProne();
+        ErrorProne errorProne = new ErrorProne();
 
 
 
--- java/PrintWord/src/PrintWord.java
+++ java/PrintWord/src/PrintWord.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class PrintWord {
-    public static void printWord(){
+    public static void printWordSwitch(){
     int number;
     Scanner sc = new Scanner(System.in);
     String[] numberList = new String[]{"ONE", "TWO", "THREE", "FOUR", "FIVE", "SIX", "SEVEN", "EIGHT", "NINE", "OTHER"};
@@ -39,6 +39,37 @@ public class PrintWord {
         default:
             System.out.printf("Number: %s%n", numberList[9]);
             break;
+        }
+
+    }
+
+    public static void printWordIfElse(){
+        int number;
+        Scanner sc = new Scanner(System.in);
+        String[] numberList = new String[]{"ONE", "TWO", "THREE", "FOUR", "FIVE", "SIX", "SEVEN", "EIGHT", "NINE", "OTHER"};
+        System.out.println("Please enter a number between one and nine ");
+        number = sc.nextInt();
+
+        if(number == 1){
+            System.out.printf("Number: %s%n", numberList[0]);
+        } else if (number == 2) {
+            System.out.printf("Number: %s%n", numberList[1]);
+        } else if (number == 3) {
+            System.out.printf("Number: %s%n", numberList[2]);
+        } else if (number == 4) {
+            System.out.printf("Number: %s%n", numberList[3]);
+        } else if (number == 5) {
+            System.out.printf("Number: %s%n", numberList[4]);
+        } else if (number == 6) {
+            System.out.printf("Number: %s%n", numberList[5]);
+        } else if (number == 7) {
+            System.out.printf("Number: %s%n", numberList[6]);
+        } else if (number == 8) {
+            System.out.printf("Number: %s%n", numberList[7]);
+        } else if (number == 9) {
+            System.out.printf("Number: %s%n", numberList[8]);
+        } else {
+            System.out.printf("Number: %s%n", numberList[9]);
         }
 
     }
