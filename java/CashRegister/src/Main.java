9931d6b14155f3891993026e0f721645a3644faa
BEABOOSS <72757983+BEABOOSS@users.noreply.github.com>
2024-11-04 22:44:50 -0500

    Cash Register completed Assignment 12-14

--- java/CashRegister/src/Main.java
+++ java/CashRegister/src/Main.java
@@ -10,29 +10,40 @@ public class Main {
     public static final String ANSI_RED = "\u001B[31m";
     public static final String ANSI_GREEN = "\u001B[32m";
     public static final String ANSI_YELLOW = "\u001B[33m";
-    public static final String ANSI_PURPLE = "\u001B[35m";
     public static final String ANSI_WHITE = "\u001B[37m";
-    public static final String ANSI_PURPLE_BACKGROUND = "\u001B[45m";
     public static final String YesNo = String.format("(" + ANSI_GREEN +"y" + ANSI_WHITE + "/" + ANSI_RED + "n" + ANSI_RESET + ")");
-    public static final String LINE_BACKGROUND_PURPLE = String.format(ANSI_PURPLE_BACKGROUND + "------------" + ANSI_RESET);
+    public static final String LINE_MENU = "------------";
 
     public static void main(String[] args){
-
+    
+        // id's start at 100 and increase by one
         ArrayList<Cashier> workers = new ArrayList<Cashier>();
-        workers.add( new Cashier("james"));
-        workers.add( new Cashier("bob"));
-        workers.add( new Cashier("burt"));
-        workers.add( new Cashier("ken"));
+        workers.add( new Cashier("James"));
+        workers.add( new Cashier("Bob"));
+        workers.add( new Cashier("Burt"));
+        workers.add( new Cashier("Ken"));
+        workers.add( new Cashier("Mary"));
+        workers.add( new Cashier("Susan"));
+        workers.add( new Cashier("David"));
+        workers.add( new Cashier("Patricia"));
 
-
+        // id's start at 100 and increase by one
         ArrayList<Item> itemList = new ArrayList<Item>();
         itemList.add(new Item("Water Melon", 5.99f, 12));
         itemList.add(new Item("Loaf of Bread", 2.50f, 35));
         itemList.add(new Item("Bag of Lemons", 3.50f, 3));
         itemList.add(new Item("Bag of Chips", 6.99f, 12));
         itemList.add(new Item("Cheese", 6.50f, 0));
+        itemList.add(new Item("Apples", 4.00f, 35));
+        itemList.add(new Item("Tomato Soup", 1.09f, 70));
+        itemList.add(new Item("Sugar", 3.49f, 6));
+        itemList.add(new Item("Peanut Butter", 7.79f, 120));
+        itemList.add(new Item("Froot Loops", 5.49f, 57));
+        itemList.add(new Item("Cream Corn", 2.29f, 103));
+        itemList.add(new Item("Oreo", 4.49f, 38));
 
-//        listItems(itemList);
+
+
         if(login(workers)){
             mainMenu(itemList);
         };
@@ -43,16 +54,17 @@ public class Main {
     public static boolean login(ArrayList<Cashier> workers){
         Scanner sc = new Scanner(System.in);
         boolean userFound = false;
-        int numOfTries = 3;
+        int NUMBER_OF_TRIES = 3;
         String cashierNumInput = "";
+        String mainMenu = String.format("%1$s Main Menu %1$s", LINE_MENU);
 
         for(int i=3; i >= 0; i--){
             if(i == 0){
-                System.out.printf(ANSI_YELLOW + "%nYOU HAVE EXCEEDED THE NUMBER OF TRIES, ARE YOU SURE YOU HAVE THE RIGHT NUMBER?%n" + ANSI_RESET);
+                System.out.printf(ANSI_YELLOW + "%n YOU HAVE EXCEEDED THE NUMBER OF TRIES,%n ARE YOU SURE YOU HAVE THE RIGHT NUMBER?%n" + ANSI_RESET);
                 break;
             }
-            System.out.printf("%n%s", i == numOfTries ? "------------Main Menu------------": "");
-            System.out.printf("%nEnter your Cashier number ("+ ANSI_YELLOW+ "%d tries left"+ANSI_RESET +"):%n", i);
+            System.out.printf("%n%s", i == NUMBER_OF_TRIES ? mainMenu : "");
+            System.out.printf("%n Enter your Cashier number ("+ ANSI_YELLOW+ "%d tries left"+ANSI_RESET +")%n :", i);
             cashierNumInput = sc.nextLine().trim();
 
             try {
@@ -62,10 +74,11 @@ public class Main {
                     throw new IllegalArgumentException(ANSI_YELLOW + "Cashier number need to be between 0 and 500" + ANSI_RESET);
                 }
 
+                // checks if cashier id exist
                 for(Cashier j : workers){
                     if(j.getCashierNumber() == cashierInput){
                         userFound = true;
-                        System.out.printf(ANSI_GREEN + "%nWELCOME BACK %s.%n" + ANSI_RESET, j.getUsername());
+                        System.out.printf(ANSI_GREEN + "%n WELCOME BACK %s.%n" + ANSI_RESET, j.getUsername());
                         break;
                     }
                 }
@@ -88,8 +101,9 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         while (!exitMenu){
 
-            System.out.printf("%n%1$s Main Menu %1$s", LINE_BACKGROUND_PURPLE);
-            System.out.printf("%n1-Add new item%n2-Purchase item%nX-Exit%n:");
+            System.out.printf("%n%1$s Main Menu %1$s", LINE_MENU);
+            System.out.printf("%n At any point you can enter "+ANSI_RED +"x" + ANSI_RESET + " to exit that menu.");
+            System.out.printf("%n 1-Add new item%n 2-Purchase item%n "+ ANSI_RED +"X-Exit" +ANSI_RESET+"%n :");
             menuSelect = sc.nextLine().trim();
 
             switch (menuSelect){
@@ -118,53 +132,59 @@ public class Main {
         Matcher matcher;
 
 
-        System.out.printf("%n%1$s Adding Item %1$s%n", LINE_BACKGROUND_PURPLE);
+        System.out.printf("%n%1$s Adding Item %1$s%n", LINE_MENU);
         listItems(itemList);
 
         while(true){
-            System.out.printf("%nEnter Item Name: ");
-            String name = sc.nextLine().trim();
-            if(name.equals("x")){
-                break;
-            }
-
-            System.out.printf("%nEnter Item Price: ");
-            String price = sc.nextLine().replace(",", ".").trim();
-            if(price.equals("x")){
-                break;
-            }
-
-            System.out.printf("%nEnter Item Quantity: ");
-            String quantity = sc.nextLine().trim();
-            if (quantity.equals("x")){
-                break;
-            }
-
             try {
-                matcher = stringVerification.matcher(name);
-                float itemPrice = Float.parseFloat(price.trim());
-                int itemQuantity = Integer.parseInt(quantity);
-
+                System.out.printf("%nEnter Item Name: ");
+                String itemName = sc.nextLine().trim();
+                if(itemName.equals("x")){
+                    break;
+                }
+                matcher = stringVerification.matcher(itemName);
 
                 if(!matcher.matches()){
                     throw new IllegalArgumentException(ANSI_RED + "Item name must only contain letters, numbers, and spaces, and start with a letter" + ANSI_RESET);
                 }
-                String itemName = name;
 
+                for(Item item : itemList){
+                    if(itemName.equals(item.getItemName())){
+                        throw new IllegalArgumentException(ANSI_YELLOW + "Item name already exist." + ANSI_RESET);
+                    }
+                }
+
+
+
+                System.out.printf("%nEnter Item Price: ");
+                String price = sc.nextLine().replace(",", ".").trim();
+                if(price.equals("x")){
+                    break;
+                }
+                float itemPrice = Float.parseFloat(price.trim());
                 if(itemPrice < 0.00f){
                     throw new IllegalArgumentException(ANSI_RED + "Item price cannot be negative" + ANSI_RESET);
                 }
+
+                // Item quantity
+                System.out.printf("%nEnter Item Quantity: ");
+                String quantity = sc.nextLine().trim();
+                if (quantity.equals("x")){
+                    break;
+                }
+
+                int itemQuantity = Integer.parseInt(quantity);
                 if(itemQuantity < 0 || itemQuantity > 200){
                     throw new IllegalArgumentException(ANSI_RED + "Item quantity cannot be negative or greater than 200" + ANSI_RESET);
                 }
 
 
-                System.out.printf(ANSI_GREEN + "%nNew Item Added: " + ANSI_RESET);
-                System.out.printf("%nItem name: %1$s%nItem price: %2$.2f%nItem quantity: %3$d", itemName, itemPrice, itemQuantity);
+                System.out.printf(ANSI_GREEN + "%n New Item Added: " + ANSI_RESET);
+                System.out.printf("%n Item name: %1$s%n Item price: %2$.2f%n Item quantity: %3$d", itemName, itemPrice, itemQuantity);
                 itemList.add(new Item(itemName, itemPrice, itemQuantity));
 
-                System.out.printf("%n%nWould you like to add another item? %s: %n", YesNo);
-                menuSelect = sc.nextLine().trim().toLowerCase();
+                System.out.printf("%n%nWould you like to add another item? %s%n:", YesNo);
+                menuSelect = sc.nextLine().trim();
 
                 if(menuSelect.equals("n")){
                     return itemList;
@@ -225,7 +245,7 @@ public class Main {
                 // checks if quantity is available
                 for (Item item : itemList) {
                     if (item.getItemId() == itemId) {
-                        if (itemQuantity >= item.getQuantity()) {
+                        if (itemQuantity > item.getQuantity()) {
                             throw new IllegalArgumentException(ANSI_RED + "Requested amount is greater than what is available currently"+ANSI_RESET);
                         } else {
                             subtotal += itemQuantity * item.getPrice();
@@ -251,8 +271,6 @@ public class Main {
                     break;
                 }
 
-
-
             } catch (NumberFormatException e){
                 System.out.println(ANSI_RED + "Error: Please enter a valid number"+ ANSI_RESET);
             } catch(IllegalArgumentException e){
@@ -267,6 +285,7 @@ public class Main {
         return itemList;
     }
 
+    // checks if item id exist or not
     public static boolean doesItemExist(ArrayList<Item> itemList, int itemId){
         for(Item item : itemList){
             if(item.getItemId() == itemId){
@@ -284,10 +303,11 @@ public class Main {
         // Print each item with fixed-width formatting
         int quantity = 0;
         for (Item item : itemList) {
+            // format the item quantity to be colored
             quantity = item.getQuantity();
-            // Format the name with fixed width
             String itemName = item.getItemName();
             String itemQuantity = String.format((quantity <= 3? ANSI_YELLOW : ANSI_WHITE)+"%d"+ANSI_RESET, quantity);
+            // Format the name with fixed width
             if (itemName.length() > ITEM_NAME_WIDTH) {
                 itemName = itemName.substring(0, ITEM_NAME_WIDTH - 3) + "...";
             }
@@ -325,14 +345,14 @@ public class Main {
 // +----- Display current items
 // +----- Prompt user to register new items
 // +------ (only stops showing prompt until 'x' is entered)
-// --- Purchase items
+// +-- Purchase items
 // +---- Display all items available
 // +---- Prompt user for item to be purchased (item number)
 // +----- Prompt user for the quantity
 // +----- Display the subtotal ( item price * quantity)
-// ------ Ask if user is finished
-// -------- yes, then Display grand total
-// ---------- then ask to return to main menu
-// -------- no, then go to line 33
+// +----- Ask if user is finished
+// +------- yes, then Display grand total
+// +--------- then ask to return to main menu
+// +------- no, then go to line 33
 
-// Finish adding colors 
+// +Finish adding colors
