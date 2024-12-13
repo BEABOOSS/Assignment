1f34c9420f096a7b9a8df577b509e929cea35337
BEABOOSS <72757983+BEABOOSS@users.noreply.github.com>
2024-12-13 17:45:34 -0500

    Assignment Submission

--- ASP_NET/CourseRegistration_/Pages/CourseList/CourseInformation.cshtml.cs
+++ ASP_NET/CourseRegistration_/Pages/CourseList/CourseInformation.cshtml.cs
@@ -1,12 +1,24 @@
-using Microsoft.AspNetCore.Mvc;
+using CourseRegistration_.Models;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace CourseRegistration_.Pages.CourseList
 {
     public class CourseInformationModel : PageModel
     {
-        public void OnGet()
+        private readonly CourseRegContext _db;
+
+        public CourseInformationModel(CourseRegContext db)
         {
+            _db = db;
+        }
+
+        public Course Course { get; set; }
+        public Instructor Instructor { get; set; }
+
+        public async Task OnGet(int id)
+        {
+            Course = await _db.Course.FindAsync(id);
+            Instructor = await _db.Instructor.FindAsync(Course.CourseId);
         }
     }
 }
